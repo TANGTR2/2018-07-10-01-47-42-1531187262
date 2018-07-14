9efe991e034fd3aa09dc4f2b8f9ba8d461cc6c1b
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-14 18:14:47 +0800

    [CollectionOperator] refactor with addAll/Arrays.stream()/boxed()/int and Integer

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -1,48 +1,64 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.*;
-import java.util.List;
 
-import static java.util.stream.Collectors.collectingAndThen;
-import static java.util.stream.Collectors.toList;
 
 public class CollectionOperator {
+    private List<Integer> getListOfReducedOfTwoNums(int leftBorder, int rightBorder, Function<Integer, Boolean> isContinue) {
+        return leftBorder < rightBorder
+                ? getListOfReducedBetweenBigAndsmallNum(leftBorder, rightBorder, isContinue)
+                : getListOfReducedBetweenBigAndsmallNum(rightBorder, leftBorder, isContinue);
+    }
+
+    private List<Integer> getListOfReducedBetweenBigAndsmallNum(int smallNum, int bigNum, Function<Integer, Boolean> isContinue) {
+        return IntStream.rangeClosed(smallNum, bigNum)
+                .filter(isContinue::apply)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
     public List<Integer> getListByInterval(int left, int right) {
-        if(left < right) {
-            List<Integer> list = IntStream.rangeClosed(left, right).boxed().collect(toList());
-            return list;
-        }else {
-            List<Integer> list = IntStream.rangeClosed(right, left).boxed()
-                    .sorted(Comparator.reverseOrder())
-                    .collect(toList());
-            return list;
-        }
+        List<Integer> reducedOfTwoNumsList = this.getListOfReducedOfTwoNums(left, right, num -> true);
+        return left < right ? reducedOfTwoNumsList : reducedOfTwoNumsList.stream()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> reducedOfTwoNumsList = this.getListOfReducedOfTwoNums(left, right, num -> num % 2 == 0);
+        return left < right ? reducedOfTwoNumsList : reducedOfTwoNumsList.stream()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream(array).boxed()
+                .filter(num -> num % 2 == 0)
+                .collect(Collectors.toList());
     }
 
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return array[array.length - 1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        return Arrays.stream(firstArray).boxed()
+                .filter(numInFirst ->
+                        Arrays.stream(secondArray).boxed()
+                                .anyMatch(numInSecond -> numInSecond.equals(numInFirst)))
+                .collect(Collectors.toList());
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> unCommonList = Arrays.stream(secondArray)
+                .filter(num -> !Arrays.stream(firstArray)
+                        .anyMatch(numInFirst -> numInFirst.equals(num)))
+                .collect(Collectors.toList());
+        List<Integer> results = Arrays.stream(firstArray).collect(Collectors.toList());
+        results.addAll(unCommonList);
+        return results;
     }
 }
