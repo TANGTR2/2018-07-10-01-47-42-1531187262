71a86ad58c8c247cc8936b58e300d725e851009a
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-14 19:12:37 +0800

    [Reduce] refactor with skip/limit/indexOf/getAsDouble

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -5,6 +5,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -35,7 +36,13 @@ public class Reduce {
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        int index = arrayList.size() / 2;
+        if (index % 2 == 0) {
+            return arrayList.stream().skip(index - 1).limit(2).mapToInt(num -> num)
+                    .average().getAsDouble();
+        } else {
+            return arrayList.stream().skip(index).findFirst().get();
+        }
     }
 
     public int getFirstEven() {
@@ -47,22 +54,38 @@ public class Reduce {
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+        int firstEvent = arrayList.stream().filter(num -> num % 2 == 0).findFirst().get();
+        return arrayList.indexOf(firstEvent);
     }
 
-    public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+    public boolean isEqual(List<Integer> comarrayList) {
+        Boolean isEqual = false;
+        if (comarrayList.size() == arrayList.size()) {
+            isEqual = IntStream.range(0, arrayList.size())
+                    .allMatch(index -> comarrayList.get(index) == arrayList.get(index));
+        }
+        return isEqual;
     }
 
-    public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
+    //实现接口SingleLink
+    public double getMedianInLinkList(SingleLink<Integer> singleLink) {
+
+        int index = arrayList.size() / 2;
+
+        if (arrayList.size() % 2 == 0) {
+            return (singleLink.getNode(index) + singleLink.getNode(index + 1)) / 2.0;
+        } else {
+            return singleLink.getNode(index + 1);
+        }
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                .filter(num -> num % 2 != 0)
+                .reduce((first, second) -> second).get();
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        return arrayList.lastIndexOf(getLastOdd());
     }
 }
